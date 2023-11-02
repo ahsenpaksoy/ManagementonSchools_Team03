21e6c0326e298e8680ace2af481ddb03daf781ab
Mustafa <mustafaarslan023447@gmail.com>
2023-11-02 23:05:32 +0100

    Mustafa 02.11 V2

---
+++ src/test/java/stepDefinitions/UiStepDefinitions/US11_StepDef.java
@@ -0,0 +1,77 @@
+package stepDefinitions.UiStepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US11_StepDef {
+    @Given("kullanici managementonschools.com sayfasina gider")
+    public void kullaniciManagementonschoolsComSayfasinaGider() {
+        
+    }
+
+    @And("kullanici Password boxa tıklar")
+    public void kullaniciPasswordBoxaTıklar() {
+        
+    }
+
+    @When("Kullanici Login Butonuna tıklar")
+    public void kullaniciLoginButonunaTıklar() {
+        
+    }
+
+    @Then("kullanici User Name boxa tıklar")
+    public void kullaniciUserNameBoxaTıklar() {
+        
+    }
+
+    @And("kullanici {int} saniye bekler")
+    public void kullaniciSaniyeBekler(int arg0) {
+        
+    }
+
+    @And("kullanıcı Login butonuna tıklar")
+    public void kullanıcıLoginButonunaTıklar() {
+        
+    }
+
+    @And("kullanıcı Lesson Program sekmesini bulur")
+    public void kullanıcıLessonProgramSekmesiniBulur() {
+        
+    }
+
+    @And("kullanici sayfayi kaydirir")
+    public void kullaniciSayfayiKaydirir() {
+        
+    }
+
+    @And("kullanici Lesson görebilmelidir.")
+    public void kullaniciLessonGörebilmelidir() {
+        
+    }
+
+    @And("kullanici Day görebilmelidir.")
+    public void kullaniciDayGörebilmelidir() {
+        
+    }
+
+    @And("kullanici Start Time görebilmelidir.")
+    public void kullaniciStartTimeGörebilmelidir() {
+        
+    }
+
+    @And("kullanici Stop Time görebilmelidir.")
+    public void kullaniciStopTimeGörebilmelidir() {
+        
+    }
+
+    @And("Kullanici Updade yapabilmelidir")
+    public void kullaniciUpdadeYapabilmelidir() {
+        
+    }
+
+    @And("Kullanici Lesson Program listesinden olusturulan dersi silebilmelidir")
+    public void kullaniciLessonProgramListesindenOlusturulanDersiSilebilmelidir() {
+    }
+}
---
+++ src/test/java/stepDefinitions/UiStepDefinitions/US16_StepDef.java
@@ -0,0 +1,67 @@
+package stepDefinitions.UiStepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US16_StepDef {
+    @Given("Kullanici ana sayfaya gider.")
+    public void kullaniciAnaSayfayaGider() {
+
+    }
+
+    @When("Kullanici Vice Dean hesabi ile giris yapar.")
+    public void kullaniciViceDeanHesabiIleGirisYapar() {
+
+    }
+
+    @And("Kullanici Menu ye  tiklar.")
+    public void kullaniciMenuYeTiklar() {
+
+    }
+
+    @Then("Kullanici  acilan pencereden Contact Get All yazisina  tiklar.")
+    public void kullaniciAcilanPenceredenContactGetAllYazisinaTiklar() {
+
+    }
+
+    @And("Kullanici Name altinda gönderici isimlerini gordugunu dogrular.")
+    public void kullaniciNameAltindaGöndericiIsimleriniGordugunuDogrular() {
+
+    }
+
+    @And("Kullanici Email altinda gönderici Email adreslerini gordugunu dogrular.")
+    public void kullaniciEmailAltindaGöndericiEmailAdresleriniGordugunuDogrular() {
+
+    }
+
+    @And("Kullanici Date altinda mesaj tarihlerini gordugunu dogrular.")
+    public void kullaniciDateAltindaMesajTarihleriniGordugunuDogrular() {
+
+    }
+
+    @And("Kullanici Subject altindaki bilgileri gordugunu dogrular.")
+    public void kullaniciSubjectAltindakiBilgileriGordugunuDogrular() {
+
+    }
+
+    @And("Kullanici Message altindaki mesajlari gordugunu dogrular.")
+    public void kullaniciMessageAltindakiMesajlariGordugunuDogrular() {
+
+    }
+
+    @And("Kullanici sayfa kapatir.")
+    public void kullaniciSayfaKapatir() {
+
+    }
+
+    @And("Kullanici silme buton  gordugunu dogrular.")
+    public void kullaniciSilmeButonGordugunuDogrular() {
+
+    }
+
+    @And("Kullanici silme butona tiklar")
+    public void kullaniciSilmeButonaTiklar() {
+    }
+}
