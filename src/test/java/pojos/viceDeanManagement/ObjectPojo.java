29dca64f65e5d3ccda28780bf03a518d0b1ca707
Sitareyn <132712069+Sitareyn@users.noreply.github.com>
2023-11-10 13:48:45 +0100

    Update ObjectPojo.java

--- src/test/java/pojos/viceDeanManagement/ObjectPojo.java
+++ src/test/java/pojos/viceDeanManagement/ObjectPojo.java
@@ -5,162 +5,163 @@ import java.io.Serializable;
 public class ObjectPojo implements Serializable {
 
 
-	private String email;
-	private String subject;
-	private String message;
-	private String date;
+    private String email;
+    private String subject;
+    private String message;
+    private String date;
 
-  private int userId;
-	private String username;
-	private String name;
-	private String surname;
-	private String birthDay;
-	private String ssn;
-	private String birthPlace;
-	private String phoneNumber;
-	private String gender;
+    private int userId;
+    private String username;
+    private String name;
+    private String surname;
+    private String birthDay;
+    private String ssn;
+    private String birthPlace;
+    private String phoneNumber;
+    private String gender;
 
-	public ObjectPojo() {
-	}
+    public ObjectPojo() {
+    }
 
-	public ObjectPojo(int userId, String username, String name, String surname, String birthDay, String ssn, String birthPlace, String phoneNumber, String gender) {
-		this.userId = userId;
-		this.username = username;
-		this.name = name;
-		this.surname = surname;
-		this.birthDay = birthDay;
-		this.ssn = ssn;
-		this.birthPlace = birthPlace;
-		this.phoneNumber = phoneNumber;
-		this.gender = gender;
-	}
+    public ObjectPojo(int userId, String username, String name, String surname, String birthDay, String ssn, String birthPlace, String phoneNumber, String gender) {
+        this.userId = userId;
+        this.username = username;
+        this.name = name;
+        this.surname = surname;
+        this.birthDay = birthDay;
+        this.ssn = ssn;
+        this.birthPlace = birthPlace;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+    }
 
-	public void setUserId(int userId){
-		this.userId = userId;
-	}
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 
-	public int getUserId(){
-		return userId;
-	}
+    public int getUserId() {
+        return userId;
+    }
 
-	public void setUsername(String username){
-		this.username = username;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public String getUsername(){
-		return username;
-	}
-
-
-	public void setName(String name){
-		this.name = name;
-	}
-
-	public String getName(){
-		return name;
-	}
+    public String getUsername() {
+        return username;
+    }
 
 
-	public void setEmail(String email){
-		this.email = email;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getEmail(){
-		return email;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setSubject(String subject){
-		this.subject = subject;
-	}
 
-	public String getSubject(){
-		return subject;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setMessage(String message){
-		this.message = message;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public String getMessage(){
-		return message;
-	}
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 
-	public void setDate(String date){
-		this.date = date;
-	}
+    public String getSubject() {
+        return subject;
+    }
 
-	public String getDate(){
-		return date;
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
-	public void setSurname(String surname){
-		this.surname = surname;
-	}
+    public String getMessage() {
+        return message;
+    }
 
-	public String getSurname(){
-		return surname;
-	}
+    public void setDate(String date) {
+        this.date = date;
+    }
 
-	public void setBirthDay(String birthDay){
-		this.birthDay = birthDay;
-	}
+    public String getDate() {
+        return date;
+    }
 
-	public String getBirthDay(){
-		return birthDay;
-	}
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 
-	public void setSsn(String ssn){
-		this.ssn = ssn;
-	}
+    public String getSurname() {
+        return surname;
+    }
 
-	public String getSsn(){
-		return ssn;
-	}
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
 
-	public void setBirthPlace(String birthPlace){
-		this.birthPlace = birthPlace;
-	}
+    public String getBirthDay() {
+        return birthDay;
+    }
 
-	public String getBirthPlace(){
-		return birthPlace;
-	}
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
 
-	public void setPhoneNumber(String phoneNumber){
-		this.phoneNumber = phoneNumber;
-	}
+    public String getSsn() {
+        return ssn;
+    }
 
-	public String getPhoneNumber(){
-		return phoneNumber;
-	}
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
 
-	public void setGender(String gender){
-		this.gender = gender;
-	}
+    public String getBirthPlace() {
+        return birthPlace;
+    }
 
-	public String getGender(){
-		return gender;
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
-	}
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-	@Override
- 	public String toString(){
-		return 
-			"ObjectPojo{" + 
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 
-			
-			",email = '" + email + '\'' + 
-			",subject = '" + subject + '\'' + 
-			",message = '" + message + '\'' + 
-			",date = '" + date + '\'' + 
+    public String getGender() {
+        return gender;
 
-			"userId = '" + userId + '\'' + 
-			",username = '" + username + '\'' + 
-			",name = '" + name + '\'' + 
-			",surname = '" + surname + '\'' + 
-			",birthDay = '" + birthDay + '\'' + 
-			",ssn = '" + ssn + '\'' + 
-			",birthPlace = '" + birthPlace + '\'' + 
-			",phoneNumber = '" + phoneNumber + '\'' + 
-			",gender = '" + gender + '\'' + 
-			"}";
-		}
+    }
+
+    @Override
+    public String toString() {
+        return
+                "ObjectPojo{" +
+
+
+                        ",email = '" + email + '\'' +
+                        ",subject = '" + subject + '\'' +
+                        ",message = '" + message + '\'' +
+                        ",date = '" + date + '\'' +
+
+                        "userId = '" + userId + '\'' +
+                        ",username = '" + username + '\'' +
+                        ",name = '" + name + '\'' +
+                        ",surname = '" + surname + '\'' +
+                        ",birthDay = '" + birthDay + '\'' +
+                        ",ssn = '" + ssn + '\'' +
+                        ",birthPlace = '" + birthPlace + '\'' +
+                        ",phoneNumber = '" + phoneNumber + '\'' +
+                        ",gender = '" + gender + '\'' +
+                        "}";
+    }
 }
