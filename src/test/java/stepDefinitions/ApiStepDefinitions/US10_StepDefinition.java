f87dbc34cd5fb49be068a900d133fafd2dcb0d15
donmezyasemin <gasemn.34@outlook.com>
2023-11-16 18:02:55 +0100

    "yasemin-16.11.23-v1"

--- src/test/java/stepDefinitions/ApiStepDefinitions/US10_StepDefinition.java
+++ src/test/java/stepDefinitions/ApiStepDefinitions/US10_StepDefinition.java
@@ -3,12 +3,23 @@ package stepDefinitions.ApiStepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import pojos.lessonManagement.ResponsePojoYD;
+import io.restassured.response.Response;
+import pojos.lessonManagement.saveLessonPorg.PostPojoYD;
+import pojos.lessonManagement.saveLessonPorg.ResponseYDPojo;
+
+import java.util.List;
 
 import static baseUrl.BaseUrl.setup;
 import static baseUrl.BaseUrl.spec;
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class US10_StepDefinition {
+    List<Integer> dersler= List.of(1929,1930);
+    PostPojoYD payload;
+    Response response;
+    ResponseYDPojo actualdata;
     @Given("{string} ile giris yapilir YD")
     public void ile_giris_yapilir_yd(String string) {
       setup("Bella", "Batch171");
@@ -20,7 +31,7 @@ public class US10_StepDefinition {
     }
     @Then("Kayitli Vice Dean icin payload duzenlenir")
     public void kayitliViceDeanIcinPayloadDuzenlenir() {
-        ResponsePojoYD payload = new ResponsePojoYD("FRIDAY",17 ,1869,);
+         payload = new PostPojoYD("FRIDAY",17 , dersler ,"12:00", "14:00");
         /*{
   "day": "MONDAY",
   "educationTermId": 0,
@@ -32,17 +43,30 @@ public class US10_StepDefinition {
 
 
     }
-    @When("Kayitli Vıce Dean icin GET Request gonderilir ve response alinir YD")
-    public void kayitli_vıce_dean_icin_get_request_gonderilir_ve_response_alinir_yd() {
-
-
+    @When("Kayitli Vıce Dean icin POST Request gonderilir ve response alinir YD")
+    public void kayitli_vıce_dean_icin_post_request_gonderilir_ve_response_alinir_yd() {
+        System.out.println(48);
+        response = given(spec).body(payload).when().post("{first}/{second}");
+        response.prettyPrint();
+        actualdata = response.as(ResponseYDPojo.class);
     }
     @Then("Kayitli Vice Dean icin gelen response dogrulanir YD")
     public void kayitliViceDeanIcinGelenResponseDogrulanirYD() {
+        assertEquals(payload.getDay(),actualdata.getObject().getDay());
+        assertTrue(payload.getLessonIdList().contains(actualdata.getObject().getLessonName().get(0).getLessonId()));
+        assertTrue(actualdata.getObject().getStartTime().contains(payload.getStartTime()));
+        assertTrue(actualdata.getObject().getStopTime().contains(payload.getStopTime()));
+//"message": "Created Lesson Program",
+//    "httpStatus": "CREATED"
+        assertEquals( "Created Lesson Program", actualdata.getMessage());
+        assertEquals( "CREATED", actualdata.getHttpStatus());
+
+
 
     }
     @Then("GetAll icin Status kodun {int} oldugu dogrulanir YD")
-    public void get_all_icin_status_kodun_oldugu_dogrulanir_yd(Integer int1) {
+    public void get_all_icin_status_kodun_oldugu_dogrulanir_yd(int statusCode) {
+        assertEquals(statusCode, response.statusCode());
 
     }
 
